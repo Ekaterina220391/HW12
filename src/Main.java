3329df3bd3a925ecfc5d1a4e331463e32087197e
origin <dubnitskaya.katya@mail.ru>
2025-11-26 08:42:15 +0400

    New

--- src/Main.java
+++ src/Main.java
@@ -5,54 +5,41 @@ public class Main {
         System.out.println("Задание 1");
     }
 
+    public static void chekLeapYear(int year) {
+        year = 2011;
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " Это високосный год");
+        } else {
+            System.out.println(year + "  Это не високосный год");
+        }
+    }
+
     public static void printTask2() {
         System.out.println("Задание 2");
+    }
+
+    public static void watchCurrentYear(int current, int client) {
+        current = 2020;
+        client = 0;
+        if (current < 2015 && client == 1) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (current < 2015 && client == 0) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (current >= 2015 && client == 1) {
+            System.out.println("Установите  приложение для iOS по ссылке");
+        } else if (current >= 2015 && client == 0) {
+            System.out.println("Установите приложение для Android по ссылке");
+        } else {
+            System.out.println("Введите корректные данные");
+        }
     }
 
     public static void printTask3() {
         System.out.println("Задание 3");
     }
 
-    public static void leapYear(int leap) {
-        int year = 2011;
-    }
-
-    public static void currentYear(int update) {
-        int current = 2020;
-    }
-
-    public static void deliveryDistance(int distance) {
-        int delivery = 110;
-    }
-
-
-    public static void main(String[] args) {
-        printTask1();
-        int year = 2011;
-        leapYear(year);
-        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-            System.out.println(year + " Это високосный год");
-        } else {
-            System.out.println(year + "  Это не високосный год");
-        }
-        printTask2();
-        int current = 2020;
-        currentYear(current);
-        int clientO = 0;
-        if (current < 2015 && clientO == 1) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (current < 2015 && clientO == 0) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (current >= 2015 && clientO == 1) {
-            System.out.println("Установите  приложение для iOS по ссылке");
-        } else if (current >= 2015 && clientO == 0) {
-            System.out.println("Установите приложение для Android по ссылке");
-        } else {
-            System.out.println("Введите корректные данные");
-        }
-        printTask3();
-        int delivery = 110;
-        deliveryDistance(delivery);
+    public static void calculateDeliveryDistance(int delivery) {
+        delivery = 110;
         int time = 1;
         if (delivery <= 20) {
             System.out.println("Потребуется дней:" + time);
@@ -60,8 +47,20 @@ public class Main {
             System.out.println("Потребуется дней:" + (time + 1));
         } else if (delivery > 60 && delivery <= 100) {
             System.out.println("Потребуется дней:" + (time + 1 + 1));
+        } else {
+            System.out.println("Доставки нет");
         }
-        return;
+    }
+
+
+
+    public static void main(String[] args) {
+        printTask1();
+        chekLeapYear(2011);
+        printTask2();
+        watchCurrentYear(2020, 0);
+        printTask3();
+        calculateDeliveryDistance(110);
 
 
     }
